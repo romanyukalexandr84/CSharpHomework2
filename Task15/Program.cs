a4085cf5f7666cfc03ec2e8f3e1d5bde8cc0f80d
romanukalexandr <romanukalexandr@gmail.com>
2023-02-14 23:19:00 +0300

    add task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿int Check (int number)
+{
+while (number < 1 || number > 7)
+{
+    Console.WriteLine ("Необходимо ввести от 1 до 7, повторите ввод");
+    number = Convert.ToInt32 (Console.ReadLine());
+}
+return number;
+}
+
+Console.WriteLine ("Введите цифру, обозначающую день недели");
+int count = Convert.ToInt32 (Console.ReadLine());
+
+if ((Check (count))<6)
+{
+    Console.WriteLine ("Это будний день");
+}
+else
+{
+    Console.WriteLine ("Это выходной");
+}
