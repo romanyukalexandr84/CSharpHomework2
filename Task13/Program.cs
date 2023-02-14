905147e16615e70fdca7dc2c6285c452e86f84db
romanukalexandr <romanukalexandr@gmail.com>
2023-02-15 01:44:34 +0300

    add task13

---
+++ Task13/Program.cs
@@ -0,0 +1,37 @@
+﻿int Check (int number)
+{
+if (number < 100)
+{
+    Console.WriteLine ("У этого числа третьей цифры нет");
+    return -1;
+}
+else
+{
+return number;
+}
+}
+
+int Third (int digit)
+{
+    int len = Convert.ToString(digit).Length;
+    int step = 1;
+    int index = 0;
+    while (index < (len-3))
+    {
+        step = step*10;
+        index++;
+    }
+    return digit / step % 10;
+}
+
+Console.WriteLine ("Введите число");
+int count = Convert.ToInt32 (Console.ReadLine());
+
+if (Check(count) == -1)
+{
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine ("Третья цифра числа = "+Third (Check (count)));
+}
