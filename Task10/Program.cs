578789a1a74005ea98e2b5a9e8153e968cfefcba
romanukalexandr <romanukalexandr@gmail.com>
2023-02-14 22:50:20 +0300

    add task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿int Check (int number)
+{
+while (number < 100 || number > 999)
+{
+    Console.WriteLine ("Число не является трехзначным, повторите ввод");
+    number = Convert.ToInt32 (Console.ReadLine());
+}
+return number;
+}
+
+int Second (int digit)
+{
+    return (digit / 10 % 10);
+}
+
+Console.WriteLine ("Введите трехзначное число");
+int count = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Вторая цифра числа = "+Second (Check (count)));
